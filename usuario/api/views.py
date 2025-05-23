6c24f6f18724739714beae4303b495198758f5f6
Andre Eduardo Pereira Pedroso <andre.pedroso@softfocus.com.br>
2025-05-23 18:49:53 -0300

    commit

--- usuario/api/views.py
+++ usuario/api/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib import messages
-from django.contrib.auth.views import PasswordResetConfirmView
-from allauth.account.views import PasswordResetFromKeyView
-from django.contrib.auth.decorators import login_required
 
 
 def sing_up_template(request):
