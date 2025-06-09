415ada81cf9c9f38853f46b75fdc78c0bc35a6d6
Andre Eduardo Pereira Pedroso <andre.pedroso@softfocus.com.br>
2025-06-09 09:56:41 -0300

    explicação e etc

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,6 +4,6 @@ from .api.views import *
 urlpatterns = [
     path("accounts/signup/", singup_view, name="account_signup"),
     path("accounts/login/", login_template, name="account_login"),
-    path("sing_up_template/", sing_up_template, name="sing-up-template"),
+    path("sign_up/", sing_up_template, name="sing-up-template"),
     path('accounts/verify-email/<str:uidb64>/<str:token>/', verify_email_view, name='account_verification_sent'),
 ]
